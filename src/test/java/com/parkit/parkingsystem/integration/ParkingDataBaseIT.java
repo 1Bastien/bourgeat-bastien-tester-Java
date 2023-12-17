0b50bd486a785ab1932f9c93d360796bfabc6eb5
Bastien <bastien.brgt@protonmail.com>
2023-12-17 18:40:11 +0100

    add testParkingLotExitRecurringUser

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,12 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -18,6 +21,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
+
+import java.util.Date;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -73,4 +78,21 @@ public class ParkingDataBaseIT {
 	assertNotNull(ticketDAO.getTicket("ABCDEF").getPrice());
     }
 
+    @Test
+    public void testParkingLotExitRecurringUser() {
+	ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+	
+	Ticket ticket = new Ticket();
+	ticket.setVehicleRegNumber("ABCDEF");
+	ticket.setInTime(new Date(System.currentTimeMillis() - (2* 60 * 60 * 1000)));
+	ticket.setOutTime(new Date(System.currentTimeMillis() - (60 * 60 * 1000)));
+	ticket.setParkingSpot(new ParkingSpot(1, ParkingType.CAR, false));
+	
+	ticketDAO.saveTicket(ticket);
+	
+	parkingService.processIncomingVehicle();
+	parkingService.processExitingVehicle();
+	
+	assertEquals(2, ticketDAO.getNbTicket("ABCDEF"));
+    }
 }
